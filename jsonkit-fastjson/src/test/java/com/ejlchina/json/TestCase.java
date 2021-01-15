d013e22354760cf6edde30ac4d5658876f7b3591
ZhouXu <zhou.xu@ejlchina.com>
2021-01-15 21:46:52 +0800

    Test Case

--- jsonkit-fastjson/src/test/java/com/ejlchina/json/TestCase.java
+++ jsonkit-fastjson/src/test/java/com/ejlchina/json/TestCase.java
@@ -1,7 +1,6 @@
 package com.ejlchina.json;
 
 
-import com.ejlchina.json.cases.User;
 import org.junit.Test;
 
 public class TestCase {
@@ -9,14 +8,6 @@ public class TestCase {
     @Test
     public void test() {
         new TestCases(new FastjsonFactory()).run();
-
-        String json = "{\"name\":\"Jack\",\"age\":20}";
-
-        User user = JSONKit.toBean(User.class, json);
-
-        System.out.println(user.getName());
-        System.out.println(user.getAge());
-
     }
 
 }
