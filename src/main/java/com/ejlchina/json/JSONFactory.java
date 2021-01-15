cc9efe977b93e29a47244166d2ae65007d1514a0
ZhouXu <zhou.xu@ejlchina.com>
2021-01-15 18:02:38 +0800

    JSON 加载机制

---
+++ src/main/java/com/ejlchina/json/JSONFactory.java
@@ -0,0 +1,16 @@
+package com.ejlchina.json;
+
+import com.ejlchina.okhttps.DataConvertor;
+
+/**
+ * JSON 工厂
+ */
+public interface JSONFactory {
+
+    /**
+     * 创建一个 JSON 转换器
+     * @return DataConvertor
+     */
+    DataConvertor create();
+
+}
